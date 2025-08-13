21234642d07df8e6139c660841f8eeedc9eda773
Niloy Chowdhury <141893834+Nemesis690@users.noreply.github.com>
2025-08-13 11:05:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -125,5 +125,5 @@ def download_history():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8080)
+
